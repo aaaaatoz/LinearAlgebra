4197ceaa1059a5106f99d83e4577a671241bdf2d
Rafa XU <rafaxu@amazon.com>
2021-04-09 22:23:53 +1000

    day1

--- main_vector.py
+++ main_vector.py
@@ -1,7 +1,22 @@
 from playLA.Vector import Vector
 
 if __name__ == "__main__":
-    vec = Vector([5,4])
+    vec = Vector([5,-3])
     print(vec)
     print(len(vec))
     print(vec[0])
+
+    vec2 = Vector([3,1])
+
+    print("{} + {} = {}".format(vec, vec2, vec2 + vec))
+    print("{} - {} = {}".format(vec, vec2, vec - vec2))
+    print("{} * {} = {}".format(vec, 3, vec * 3))
+    print("{} * {} = {}".format(3, vec, 3 * vec))
+
+    print("+{} = {}".format(vec, +vec))
+    print("-{} = {}".format(vec, -vec))
+
+    zero2 = Vector.zero(2)
+    print(zero2)
+
+    print("{} + {} = {}".format(vec, zero2, vec+zero2))
--- playLA/Vector.py
+++ playLA/Vector.py
@@ -1,10 +1,38 @@
 class Vector:
 
     def __init__(self, lst):
-        self._values = lst
+        self._values = list(lst)
+
+    @classmethod
+    def zero(cls, dim):
+        return cls([0] * dim)
+
+
+    def __add__(self, another):
+        assert len(self) == len(another), "Error adding as length of vectors must be same"
+        return Vector([a + b for a, b in zip(self, another)])
+
+    def __sub__(self, another):
+        assert len(self) == len(another), "Error adding as length of vectors must be same"
+        return Vector([a - b for a, b in zip(self, another)])
+
+    def __mul__(self, k):
+        return Vector([k * element for element in self])
+
+    def __rmul__(self, k):
+        return self * k
+
+    def __iter__(self):
+        return self._values.__iter__()
 
     def __repr__(self):
         return "Vector({})".format(self._values)
+
+    def __pos__(self):
+        return self
+
+    def __neg__(self):
+        return -1 * self
 
     def __str__(self):
         return "({})".format(", ".join(str(e) for e in self._values))
